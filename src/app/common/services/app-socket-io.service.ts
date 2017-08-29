ed040c72cecd524debe9c854da8488e56992eb63
benma16 <benmamiya@gmail.com>
2017-08-30 02:09:29 +0300

    socket service

---
+++ src/app/common/services/app-socket-io.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import * as io from 'socket.io-client';
+import {AdModel} from "../../models/ad.model";
+import {Observable} from "rxjs";
+
+@Injectable()
+export class AppSocketIoService {
+
+  private socket: SocketIOClient.Socket;
+
+  constructor() {
+    this.socket = io();
+  }
+
+  emitAdsUpdated(ads: AdModel[]) {
+    this.socket.emit('adsGridUpdated', ads);
+  }
+
+  onAdsUpdated(): Observable<any> {
+    return Observable.create(observer=>{
+      this.socket.on('adsUpdated', (ads: AdModel[]) => {
+        console.log('ads: ', ads);
+        observer.next(ads);
+
+      });
+    });
+  }
+}
